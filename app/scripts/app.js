7ffb9da0ee26e41031c004df69f5779bb346a2df
montesq <montesq@aliceadsl.fr>
2013-07-13 20:11:05 +0200

    add dropdown support

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,10 @@
 'use strict';
 
-angular.module('fastApp', [])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+var app = angular.module('myApp', ['auth','menu', 'ui.bootstrap.dropdownToggle']).
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
   }]);
+
+app.run(function($rootScope) {
+  $rootScope.sterapiServer = "http://localhost:9000"
+});
