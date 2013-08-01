87ee9d8ed559d992ce020b7c97ffa5c94a75c336
montesq <montesq@aliceadsl.fr>
2013-08-01 23:08:50 +0200

    clients list view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,14 @@
 'use strict';
 
-var app = angular.module('myApp', ['auth','menu', 'clients']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+var app = angular.module('myApp', ['auth','menu', 'clients', 'restangular']).
+  config(['$routeProvider',
+          '$locationProvider',
+          'RestangularProvider',
+          function($routeProvider, $locationProvider, RestangularProvider) {
     $locationProvider.html5Mode(true);
+
+    RestangularProvider.setBaseUrl('http://localhost:9000/api');
+    RestangularProvider.setRestangularFields({id: '_id'});
   }]);
 
 app.run(function($rootScope) {
--- app/scripts/controllers/clients.js
+++ app/scripts/controllers/clients.js
@@ -3,19 +3,39 @@
 var app = angular.module('clients', ['ngGrid']).
     config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
-        when('/clients', {templateUrl: 'views/clients/list.html', controller: ClientsListCtrl}).
+        when('/clients', {templateUrl: 'views/clients/list.html', controller: 'ClientsListCtrl'}).
         otherwise({redirectTo:'/'});
 //        when('/clients/add', {templateUrl: 'views/clients/add.html', controller: 'clientsAddCtrl'}).
 //        when('/clients/:id', {templateUrl: 'views/clients/detail.html', controller: 'clientDetailCtrl'});
 }]);
 
-function ClientsListCtrl($scope) {
-    $scope.myData = [
-        {name: "Moroni", age: 50},
-        {name: "Tiancum", age: 43},
-        {name: "Jacob", age: 27},
-        {name: "Nephi", age: 29},
-        {name: "Enos", age: 34}];
-    $scope.gridOptions = { data : 'myData' };
-}
+app.controller('ClientsListCtrl', function($scope, Restangular) {
+    $scope.myData = Restangular.all("accounts").getList();
 
+    $scope.columnsDef = [
+        {
+            field: 'name',
+            displayName: 'Nom du client',
+            cellTemplate: '<a href="/clients/{{row.getProperty(\'_id\')}}">{{row.getProperty(col.field)}}</a>'
+        },
+        {
+            field: 'contacts',
+            displayName: 'Contacts',
+            cellTemplate: '<ul><li ng-repeat="contact in row.getProperty(col.field)">{{contact}}</li></ul>'
+        },
+        {
+            field: 'modified_on',
+            displayName: 'Dernière modif',
+            cellTemplate: '<span>{{row.getProperty(col.field)| date:"shortDate"}}</span>'
+        }
+    ];
+
+    $scope.gridOptions = {
+        data : 'myData',
+        columnDefs: 'columnsDef',
+        rowHeight: 60,
+        enableRowSelection: false,
+        showFooter: true,
+        showFilter: true
+    };
+});
